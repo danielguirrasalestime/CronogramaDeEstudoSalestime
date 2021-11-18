74eb6ec46c11836e8b99ea4ef4ee20bf2e25aafe
danielguirrasalestime <94464089+danielguirrasalestime@users.noreply.github.com>
2021-11-18 15:01:16 -0300

    cap  8 and 20

---
+++ eloquent/cap8/error.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function Person(name) {
+  this.name = name;
+}
+let ferdinand = Person("Ferdinand"); // oops
+console.log(name);
+// → Ferdinand
---
+++ eloquent/cap8/try.js
@@ -0,0 +1,53 @@
+//Exemplo de transferencia bancaria
+
+const accounts = {
+  a: 100,
+  b: 0,
+  c: 20,
+};
+
+function getAccount() {
+  let accountName = prompt("Enter an account name");
+  if (!accounts.hasOwnProperty(accountName)) {
+    throw new Error(`No such account: ${accountName}`);
+  }
+  return accountName;
+}
+function transfer(from, amount) {
+  if (accounts[from] < amount) return;
+  let progress = 0;
+  try {
+    accounts[from] -= amount;
+    progress = 1;
+    accounts[getAccount()] += amount;
+    progress = 2;
+  } finally {
+    //Diferente do catch não importa oque ocorreu no try o finally sempre executa
+    if (progress == 1) {
+      accounts[from] += amount;
+    }
+  }
+}
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure)) throw e;
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
